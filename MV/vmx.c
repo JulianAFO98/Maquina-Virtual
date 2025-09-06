a8e480c38c110a86cf394ab55500e7f078eb5cee
julian <gonna1560@gmail.com>
2025-09-06 20:01:52 -0300

    cambios en operandos

--- MV/vmx.c
+++ MV/vmx.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
     TVM VM;
     int error = 0;
-    uint32_t direccionFisicaIP, cont = 0;
+    uint32_t direccionFisicaIP;
     if (argc >= 2 && argc < 4)
     {
         if (comprobarExtension(argv[1])) // comprueba primero que sea .vmx, asi no lee un .txt por ejemplo
@@ -25,39 +25,47 @@ int main(int argc, char *argv[])
                 inicializarTablaDescriptores(&VM);
                 while (VM.registros[3] != 0xF && !error)
                 {                                                                             //
-                   direccionFisicaIP = obtenerDireccionFisica(&VM, VM.registros[3], &error); // obtener instruccion a partir de la IP Logica Reg[3] es el reg IP
-                   interpretaInstruccion(&VM, VM.memoria[direccionFisicaIP]);
-
+                    direccionFisicaIP = obtenerDireccionFisica(&VM, VM.registros[3], &error); // obtener instruccion a partir de la IP Logica Reg[3] es el reg IP
+                    interpretaInstruccion(&VM, VM.memoria[direccionFisicaIP]);
 
                     uint8_t op1 = (VM.registros[5] >> 24) & 0x3;
                     uint8_t op2 = (VM.registros[6] >> 24) & 0x3;
-                    //Debug por las dudas
-                    printf("IP logico: 0x%08X | Dir. fisica: 0x%08X | Instruccion: 0x%08X | op1=%d op2=%d\n", VM.registros[3], direccionFisicaIP, VM.memoria[direccionFisicaIP], op1, op2);
-                    printf("0x%08X 0x%08X\n", VM.registros[5], VM.registros[6]);
-                    //Valido que haya algun operando
+                    // Debug por las dudas
+                    // printf("IP logico: 0x%08X | Dir. fisica: 0x%08X | Instruccion: 0x%08X | op1=%d op2=%d\n", VM.registros[3], direccionFisicaIP, VM.memoria[direccionFisicaIP], op1, op2);
+                    // printf("0x%08X 0x%08X\n", VM.registros[5], VM.registros[6]);
+                    // Valido que haya algun operando
                     if ((op1 != 0) && (op2 != 0))
                     {
-                        uint32_t auxDireccion = direccionFisicaIP; //Variable auxiliar para no operar directamente desde direccionFisicaIP
+                        uint32_t auxDireccion = direccionFisicaIP; // Variable auxiliar para no operar directamente desde direccionFisicaIP
                         if (op2 != 0)
                         {
-                            VM.registros[6] = cargarOperando(VM.registros[6], VM.memoria, direccionFisicaIP , op2); 
-                            auxDireccion += op2; 
+                            VM.registros[6] = cargarOperando(VM.registros[6], VM.memoria, direccionFisicaIP, op2);
+                            auxDireccion += op2;
                         }
 
                         if (op1 != 0)
                         {
-                            VM.registros[5] = cargarOperando(VM.registros[5], VM.memoria, direccionFisicaIP+op2, op1);
+                            VM.registros[5] = cargarOperando(VM.registros[5], VM.memoria, direccionFisicaIP + op2, op1);
                         }
                     }
-                    printf("0x%08X 0x%08X\n", VM.registros[5], VM.registros[6]);
-                    // Operandos listos para operar
-                    // operaciones[VM.registros[4]](&VM);
-                    //printf("%s\n", operacionDessambler(VM.registros[4]));
+                    else
+                    {
+                        if ((op1 == 0) && (op2 != 0))
+                        {
+                            VM.registros[5] = VM.registros[6];
+                            VM.registros[6] = 0x0;
+                            VM.registros[5] = cargarOperando(VM.registros[5], VM.memoria, direccionFisicaIP, op1);
+                        }
+                    }
+                    // printf("0x%08X 0x%08X\n", VM.registros[5], VM.registros[6]);
+                    //  Operandos listos para operar
+                    //  operaciones[VM.registros[4]](&VM);
+                    printf("%s\n", operacionDessambler(VM.registros[4]));
                     // Si hay algun JUMP cambiamos Ip sino sumamos como esta abajo
                     VM.registros[3] += obtenerSumaBytes(&VM) + 1; // Sumamos al IP los bytes a leer mas 1
-                    cont++;
                 }
-                if(error == 1){
+                if (error == 1)
+                {
                     printf("Segmentation fault");
                 }
             }
